ab3e8c573130d8bc346ff7658fab7e5916a4040c
Robert Chu <RobChooses@users.noreply.github.com>
2024-03-16 13:53:06 +0000

    Add Dynammic and Scaffold Eth 2

---
+++ scaffold-eth-2/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useEffect } from "react";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { Toaster } from "react-hot-toast";
+import { Footer } from "~~/components/Footer";
+import { Header } from "~~/components/Header";
+import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
+import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
+import { useDarkMode } from "~~/hooks/scaffold-eth/useDarkMode";
+import scaffoldConfig from "~~/scaffold.config";
+import { useGlobalState } from "~~/services/store/store";
+
+// Setting up list of evmNetworks
+const evmNetworks = [
+  {
+    blockExplorerUrls: ["http://spicy-explorer.chiliz.com/"],
+    chainId: 88882,
+    chainName: "Chiliz Spicy Testnet",
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/polygon.svg"],
+    name: "Chiliz",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Chiliz",
+      symbol: "CHZ",
+    },
+    networkId: 88882,
+    rpcUrls: ["https://spicy-rpc.chiliz.com"],
+    vanityName: "Chiliz",
+  },
+];
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  const price = useNativeCurrencyPrice();
+  const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
+
+  useEffect(() => {
+    if (price > 0) {
+      setNativeCurrencyPrice(price);
+    }
+  }, [setNativeCurrencyPrice, price]);
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="relative flex flex-col flex-1">{children}</main>
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+};
+
+export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
+  const { isDarkMode } = useDarkMode();
+
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: scaffoldConfig.dynamicEnvId,
+        walletConnectors: [EthereumWalletConnectors],
+        evmNetworks,
+      }}
+      theme={isDarkMode ? "dark" : "light"}
+    >
+      <ProgressBar />
+      <DynamicWagmiConnector>
+        <ScaffoldEthApp>{children}</ScaffoldEthApp>
+      </DynamicWagmiConnector>
+    </DynamicContextProvider>
+  );
+};
---
+++ scaffold-eth-2/packages/nextjs/next.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    },
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
