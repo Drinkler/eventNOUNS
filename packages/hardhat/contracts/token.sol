fcb80869f787664ffae64fb905fca40549ab8e11
Florian Drinkler <flo.drinkler@gmail.com>
2024-03-16 17:47:16 +0000

    fan token deployment

--- packages/hardhat/contracts/token.sol
+++ packages/hardhat/contracts/token.sol
@@ -2,57 +2,80 @@
 pragma solidity ^0.8.16;
 
 interface IERC20 {
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address to, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+	event Transfer(address indexed from, address indexed to, uint256 value);
+	event Approval(
+		address indexed owner,
+		address indexed spender,
+		uint256 value
+	);
+	function totalSupply() external view returns (uint256);
+	function balanceOf(address account) external view returns (uint256);
+	function transfer(address to, uint256 amount) external returns (bool);
+	function allowance(
+		address owner,
+		address spender
+	) external view returns (uint256);
+	function approve(address spender, uint256 amount) external returns (bool);
+	function transferFrom(
+		address from,
+		address to,
+		uint256 amount
+	) external returns (bool);
 }
 
 contract SampleERC20Token is IERC20 {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+	string public symbol;
+	string public name;
+	uint8 public decimals;
+	uint public _totalSupply;
+	mapping(address => uint) balances;
+	mapping(address => mapping(address => uint)) allowed;
 
-    constructor() {
-        symbol = "SET";
-        name = "Sample ERC20 Token";
-        decimals = 2;
-        _totalSupply = 100000;
-        balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
-    }
-    function totalSupply() external view returns (uint) {
-        return _totalSupply  - balances[address(0)];
-    }
-    function balanceOf(address tokenOwner) external view returns (uint balance) {
-        return balances[tokenOwner];
-    }
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = balances[msg.sender] - tokens;
-        balances[to] = balances[to] + tokens;
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = balances[from] - tokens;
-        allowed[from][msg.sender] = allowed[from][msg.sender] - tokens;
-        balances[to] = balances[to] + tokens;
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
+	constructor(string memory _name, string memory _symbol) {
+		name = _name;
+		symbol = _symbol;
+		decimals = 0;
+		_totalSupply = 100000 * 10 ** uint(decimals);
+		balances[msg.sender] = _totalSupply;
+		emit Transfer(address(0), msg.sender, _totalSupply);
+	}
+	function totalSupply() external view returns (uint) {
+		return _totalSupply - balances[address(0)];
+	}
+	function balanceOf(
+		address tokenOwner
+	) external view returns (uint balance) {
+		return balances[tokenOwner];
+	}
+	function transfer(address to, uint tokens) public returns (bool success) {
+		balances[msg.sender] = balances[msg.sender] - tokens;
+		balances[to] = balances[to] + tokens;
+		emit Transfer(msg.sender, to, tokens);
+		return true;
+	}
+	function approve(
+		address spender,
+		uint tokens
+	) public returns (bool success) {
+		allowed[msg.sender][spender] = tokens;
+		emit Approval(msg.sender, spender, tokens);
+		return true;
+	}
+	function transferFrom(
+		address from,
+		address to,
+		uint tokens
+	) public returns (bool success) {
+		balances[from] = balances[from] - tokens;
+		allowed[from][msg.sender] = allowed[from][msg.sender] - tokens;
+		balances[to] = balances[to] + tokens;
+		emit Transfer(from, to, tokens);
+		return true;
+	}
+	function allowance(
+		address tokenOwner,
+		address spender
+	) external view returns (uint remaining) {
+		return allowed[tokenOwner][spender];
+	}
 }
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,58 +1,105 @@
-import Link from "next/link";
+"use client";
+
+import React, { useState } from "react";
+import SampleERC20Token from "../../hardhat/artifacts/contracts/token.sol/SampleERC20Token.json";
+import { ethers } from "ethers";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const Home: NextPage = () => {
+  const [tokenName, setTokenName] = useState("");
+  const [tokenSymbol, setTokenSymbol] = useState("");
+
+  const deployToken = async () => {
+    const rpcUrl = "https://spicy-rpc.chiliz.com/";
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+
+    const privateKey = "0x6003dbf575e0796ea69d6f1ad95c9715a8bdac13d3a8199401c183b2458741b1";
+    const signer = new ethers.Wallet(privateKey, provider);
+
+    const contractFactory = new ethers.ContractFactory(SampleERC20Token.abi, SampleERC20Token.bytecode, signer);
+    const contract = await contractFactory.deploy(tokenName, tokenSymbol);
+
+    console.log("Waiting for Deployment");
+
+    await contract.waitForDeployment();
+
+    console.log("Contract deployed to:", contract.getAddress());
+  };
+
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">EventNOUNS DAO</span>
           </h1>
           <p className="text-center text-lg">
-            Get started by editing{" "}
+            Jumpstart your journey with EventNOUNS by customizing{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
+              packages/nextjs/app/pages/index.tsx
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Craft your DAO's governance and events smart contracts in{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
+              packages/hardhat/contracts/EventNouns.sol
             </code>
           </p>
         </div>
+      </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+      <div className="bg-base-300 w-full mt-16 px-8 py-12 flex flex-wrap justify-center gap-8">
+        <div className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl">
+          <img
+            src="https://noun-api.com/beta/pfp"
+            alt="Description of First Image"
+            className="h-32 w-32 object-cover rounded-full mb-4"
+          />
+          <p>Description or caption for the first image</p>
         </div>
+        <div className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl">
+          <img
+            src="https://noun-api.com/beta/pfp"
+            alt="Description of Second Image"
+            className="h-32 w-32 object-cover rounded-full mb-4"
+          />
+          <p>Description or caption for the second image</p>
+        </div>
+        <div className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl">
+          <img
+            src="https://noun-api.com/beta/pfp"
+            alt="Description of Third Image"
+            className="h-32 w-32 object-cover rounded-full mb-4"
+          />
+          <p>Description or caption for the third image</p>
+        </div>
+        <div className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl">
+          <img
+            src="https://noun-api.com/beta/pfp"
+            alt="Description of Fourth Image"
+            className="h-32 w-32 object-cover rounded-full mb-4"
+          />
+          <p>Description or caption for the fourth image</p>
+        </div>
+      </div>
+
+      <div className="mt-8 text-center">
+        <input
+          className="mr-2 border-2"
+          placeholder="Token Name"
+          value={tokenName}
+          onChange={e => setTokenName(e.target.value)}
+        />
+        <input
+          className="mr-2 border-2"
+          placeholder="Token Symbol"
+          value={tokenSymbol}
+          onChange={e => setTokenSymbol(e.target.value)}
+        />
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={deployToken}>
+          Create Token
+        </button>
       </div>
     </>
   );
